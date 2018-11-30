735a29ddebfa92915fe3e16bb5a482a0249f2cee
Jeff Hykin <jeffhykin@Jeffs-MacBook-Pro-2.local>
2018-11-30 13:51:33 -0600

    modular update

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-global.server = require("quik-server")
+global.server   = require("quik-server")
+
+// plug-ins
+let quikDom     = server.quikAdd("quik-dom")     // this does the JSX magic
+let quikBackend = server.quikAdd("quik-backend") // this does the backend/api magic
 
 server.settings = {
     port: 3000,
     websiteFile: "./website.jsx",
+    middlewareSetup: () => {
+        quikBackend(server)
+    }
 }
 
 server.start()
