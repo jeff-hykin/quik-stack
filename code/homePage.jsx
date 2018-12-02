6d36e8c70bef1af7a45cc39e314bc5c434dbf75f
Jeff Hykin <jeffhykin@jeffs-mbp-2.lan>
2018-12-01 20:06:47 -0600

    remove leftover argument

--- code/homePage.jsx
+++ code/homePage.jsx
@@ -1,7 +1,7 @@
 module.exports = <body>
     <h1>Welcome to the Future</h1>
     <h5>Care to join me?</h5>
-    <a class="waves-effect waves-light btn" onclick={()=>history.loadPage("login", true)}>
+    <a class="waves-effect waves-light btn" onclick={()=>history.loadPage("login")}>
         Go to Login
     </a>
 </body>
