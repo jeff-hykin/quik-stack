b2ea4ec03bb4a5b4ef3f37ee475889832bc508f4
Jeff Hykin <jeffhykin@jeffs-mbp-2.lan>
2019-01-21 00:18:13 -0600

    coodinate update with quik-server

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-global.server   = require("quik-server")
+let app   = require("quik-server")
 
 // plug-ins
-let quikBackend     = server.quikAdd("quik-backend")     // this does the backend/api magic
-let quikDom         = server.quikAdd("quik-dom")         // this does the JSX magic
-let quikPageManager = server.quikAdd("quik-history")     // this adds the history.loadPage to the frontend
+let quikBackend     = app.quikAdd("quik-backend")     // this does the backend/api magic
+let quikDom         = app.quikAdd("quik-dom")         // this does the JSX magic
+let quikPageManager = app.quikAdd("quik-history")     // this adds the history.loadPage to the frontend
 
 // Here are some css frameworks (only comment-in one at a time though)
-// let quikMaterialize = server.quikAdd("quik-materialize") // this adds the materialize-css framework to the frontend
-// let quikBootstrap   = server.quikAdd("quik-bootstrap")   // this adds the bootstrap-css framework to the frontend
+// let quikMaterialize = app.quikAdd("quik-materialize") // this adds the materialize-css framework to the frontend
+// let quikBootstrap   = app.quikAdd("quik-bootstrap")   // this adds the bootstrap-css framework to the frontend
 
-server.settings = {
+app.settings = {
+    // default settings (all are optional)
+    host: 'localhost',
     port: 3000,
     websiteFile: "./website.jsx",
-    middlewareSetup: () => {
-        quikBackend(server)
-    }
+    codeFolder: "./code",
+    computerGeneratedFolder: "./computer-generated-code",
+    bundlerOptions: {}, // see https://parceljs.org/api.html for options
+    afterSystemMiddlewareSetup: () => {
+        // anything you want to do
+    },
+    afterSystemFrontendSetup: () => {
+        // anything you want to do
+    },
+    afterSystemBundlerSetup: () => {
+        // anything you want to do
+    },
+    afterServerStarted: () => {
+        // anything you want to do
+    },
 }
-
-server.start()
+app.start()
