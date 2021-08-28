37abae2c1b341bc4bc7b636d03b0a73f51832aa6
Jeff Hykin <jeff.hykin@gmail.com>
2021-08-28 14:02:00 -0500

    -

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 let app   = require("quik-server")
 
 // plug-ins
-let quikBackend     = app.quikAdd("quik-backend")     // this does the backend/api magic
-let quikDom         = app.quikAdd("quik-dom")         // this does the JSX magic
-let quikPageManager = app.quikAdd("quik-history")     // this adds the history.loadPage to the frontend
+app.quikAdd("quik-backend")         // this does the backend/api magic
+app.quikAdd("quik-dom")             // this does the JSX magic
+app.quikAdd("quik-history")         // this adds the history.loadPage to the frontend
+app.quikAdd("quik-module-template") // this is for developing/testing new quik modules
 
 // Here are some css frameworks (only comment-in one at a time though)
-// let quikMaterialize = app.quikAdd("quik-materialize") // this adds the materialize-css framework to the frontend
-// let quikBootstrap   = app.quikAdd("quik-bootstrap")   // this adds the bootstrap-css framework to the frontend
+// app.quikAdd("quik-materialize") // this adds the materialize-css framework to the frontend
+// app.quikAdd("quik-bootstrap")   // this adds the bootstrap-css framework to the frontend
 
 app.settings = {
     // default settings (all are optional)
@@ -15,7 +16,7 @@ app.settings = {
     port: 3000,
     websiteFile: "./website.jsx",
     codeFolder: "./code",
-    computerGeneratedFolder: "./computer-generated-code",
+    computerGeneratedFolder: "./computerGeneratedCode",
     bundlerOptions: {}, // see https://parceljs.org/api.html for options
     afterSystemMiddlewareSetup: () => {
         // anything you want to do
@@ -28,6 +29,7 @@ app.settings = {
     },
     afterServerStarted: () => {
         // anything you want to do
+        console.log(`Server running on http://${app.settings.host}:${app.settings.port}`)
     },
 }
 app.start()
